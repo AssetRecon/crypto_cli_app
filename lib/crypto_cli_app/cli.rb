449152734700a727e08ddc5b486b3911dce44d29
Edsel Caprice <caprice@mail.usf.edu>
2018-07-21 21:47:37 +0000

    update CLI interface to clear error

--- lib/crypto_cli_app/cli.rb
+++ lib/crypto_cli_app/cli.rb
@@ -40,14 +40,18 @@ class CryptoCliApp::CLI
 
  def menu
    #ask user to select a currency to dril down then
-   puts "Which currency do you want more info on? Enter 1 - 25"
+   puts "Which currency do you want more info on? Enter 1 - 25. Type Exit to leave."
    input = gets.strip
    valid_input?(input)
  end
 
  def valid_input?(input)
    #checks that input is between 1 - 25
-   input.to_i > 0 && input.to_i < 26 ? puts "calls method that shows more detail" : puts "Please enter a valid number 1 - 25."
+   if (input.to_i > 0 && input.to_i < 26)
+      puts "calls method that shows more detail"
+    else 
+      puts "Please enter a valid number 1 - 25."
+    end
  end
 
  def goodbye
