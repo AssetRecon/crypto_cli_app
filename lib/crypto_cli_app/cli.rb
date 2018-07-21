c304fa7222e1f2ef3bb17fb65adf463532160726
Edsel Caprice <caprice@mail.usf.edu>
2018-07-21 22:48:21 +0000

    cli control flow corrected

--- lib/crypto_cli_app/cli.rb
+++ lib/crypto_cli_app/cli.rb
@@ -43,12 +43,11 @@ class CryptoCliApp::CLI
    input = nil
      puts "Which currency do you want more info on? Enter: 1 - 25. Type exit to leave."
      input = gets.strip.downcase
-     valid_input?(input) ? detail(input) : goodbye
-   
+     valid_input?(input) ? detail(input) : invalid_or_exit(input)
  end
 
  def valid_input?(input)
-   #checks that input is between 1 - 25
+   #checks that input is between 1 - 25. Returns true or false.
    if (input.to_i > 0 && input.to_i < 26)
       true
     else
@@ -56,8 +55,16 @@ class CryptoCliApp::CLI
     end
  end
 
- def invalid
+ def invalid_or_exit(input)
+   #If input is not between 1 - 25, is it "exit" or an invalid number?
+   if input == "exit"
+     goodbye
+   else
    puts "Please enter a valid number 1 - 25."
+   menu
+  end
+ end
+ 
 
  def goodbye
    puts "Check back later for updated prices. Bye."
@@ -87,9 +94,19 @@ class CryptoCliApp::CLI
 
  def detail_two(input)
    #displays crypto's price
-   puts "2nd level of data"
+   puts "2nd level of data. Please enter back or exit"
+   new_input = gets.strip.downcase
+   back_or_exit(new_input)
  end
 
+ def back_or_exit(input)
+   case input
+  when "exit"
+    goodbye
+  when "back"
+    call
+  end
+ end
  
 
 end
