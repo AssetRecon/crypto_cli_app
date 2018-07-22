d35f4a7c8c4c763d314f9b6b3a5f4863f4e5c5b0
Edsel Caprice <caprice@mail.usf.edu>
2018-07-22 00:42:03 +0000

    add scraper methods

--- lib/crypto_cli_app/scraper.rb
+++ lib/crypto_cli_app/scraper.rb
@@ -4,6 +4,14 @@ require 'nokogiri'
 
 class CryptoCliApp::Scraper
 
+  def self.scrape_top_25(index_url)
+    array = []
+    doc =  Nokogiri::HTML(open(index_url))
+  end
+
+  def scrape_coin_details(coin_url)
+
+  end
 
 
 
