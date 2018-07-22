bc0426587e55910b3bcc6fbbf0c2de863d19e923
Edsel Caprice <caprice@mail.usf.edu>
2018-07-22 03:08:20 +0000

    fixed iterator in scraper

--- lib/crypto_cli_app/scraper.rb
+++ lib/crypto_cli_app/scraper.rb
@@ -5,20 +5,18 @@ class Scraper
 
   def self.scrape_top_25(index_url)
     array = []
-    counter = 0
     doc =  Nokogiri::HTML(open(index_url))
-    doc.css("div.container table.table tbody tr").each do |coin|
-      while counter < 25
-        counter += 1
+    doc.css("div.container table.table tbody tr")[0..24].each do |coin|
       array << {coin: coin.css("td.no-wrap a.currency-name-container").text,
                 coin_url: coin.css("td.no-wrap a.currency-name-container").attribute("href").value ,
-                price: coin.css("td.no-wrap")[2].text,
-                volume: coin.css("td.no-wrap")[3].text,
-                market_cap: coin.css("td.no-wrap")[1].text ,
-                change_percentage: coin.css("td.no-wrap")[5].text
-                }
-              end
-    end
+                price: coin.css("td.no-wrap a.price").text,
+                volume: coin.css("td.no-wrap a.volume").text,
+                market_cap: coin.css("td.no-wrap.market-cap").text ,
+                change_percentage: coin.css("td.no-wrap.percent-change").text
+            }
+      end
+
+
     #binding.pry
   puts array
   end
