ce1e48d31128ef9cd0a1b908bf056b2d33541c3e
Edsel Caprice <caprice@mail.usf.edu>
2018-07-22 04:15:24 +0000

    replace mock cli with real functionality

--- lib/crypto_cli_app/crypto.rb
+++ lib/crypto_cli_app/crypto.rb
@@ -2,8 +2,10 @@
 class CryptoCliApp::Crypto
   attr_accessor :price, :pair, :volume, :exchange, :market_cap, :change_percentage, :coin, :coin_url
 
+  @@all = []
   def initialize(attribute_hash = nil)
    attribute_hash.each {|key, value| self.send(("#{key}="), value)} if attribute_hash != nil
+   @@all << self
   end
 
   def self.create_from_collection(coin_array)
@@ -13,33 +15,8 @@ class CryptoCliApp::Crypto
 
 
   def top_crypto #will new to scrape this data from CoinMarketCap.com
-    puts <<-DOC
-    1	Bitcoin
-    2	Ethereum
-    3	XRP
-    4	Bitcoin Cash
-    5	EOS
-    6	Stellar
-    7	Litecoin
-    8	Cardano
-    9	IOTA
-    10	Tether
-    11	TRON
-    12	NEO
-    13	Monero
-    14	Dash
-    15	Ethereum Classic
-    16	NEM
-    17	Tezos
-    18	Binance Coin
-    19	VeChain
-    20	OmiseGO
-    21	Zcash
-    22	Qtum
-    23	Bitcoin Diamond
-    24	0x
-    25	Decred
-    DOC
+    i = 0
+    @@all[0..24].each {|crypto| puts "#{i += 1}. #{crypto.coin}"}
   end
 
   def crypto_exchange
