590b9e1eb06ef80403676ae35bf100146f9c927d
Edsel Caprice <caprice@mail.usf.edu>
2018-07-22 05:18:00 +0000

    add 2nd level functionality

--- lib/crypto_cli_app/crypto.rb
+++ lib/crypto_cli_app/crypto.rb
@@ -1,7 +1,7 @@
 
 class CryptoCliApp::Crypto
-  attr_accessor :price, :pair, :volume, :exchange_1, :exchange_2, :market_cap, :change_percentage, :coin, :coin_url, :selection
-
+  attr_accessor :price, :pair, :volume, :exchange_1, :exchange_2, :market_cap, :change_percentage, :coin, :coin_url, :exchange_1_price, :exchange_2_price
+  @@selection = nil
   @@all = []
   def initialize(attribute_hash = nil)
    attribute_hash.each {|key, value| self.send(("#{key}="), value)} if attribute_hash != nil
@@ -21,7 +21,7 @@ class CryptoCliApp::Crypto
 
   def crypto_exchange(input)
     #displays average price, market cap, 24h volume, change%. Also offers user to drill down on top two exchanges to view most popular trading pairs
-    @selection = @@all[input.to_i-1]
+    @@selection = input
     puts "#{@@all[input.to_i-1].coin} is currently being traded for an average price of #{@@all[input.to_i-1].price}"
     puts "Current Market Capitalization: #{@@all[input.to_i-1].market_cap}"
     puts "24 Hour Trading Volume: #{@@all[input.to_i-1].volume}"
@@ -35,8 +35,11 @@ class CryptoCliApp::Crypto
   def crypto_exchange_price(input)
     #displays trading price, volume, and trading pair for the selected exchange
     puts "This coin is currently trading at $1.25 USD on this exchange"
-    @selection.exchange_1_price
-    @selection.exchange_2_price
+    if input == "1"
+      puts "On #{@@all[@@selection.to_i-1].exchange_1}, #{@@all[@@selection.to_i-1].coin} is being traded at #{@@all[@@selection.to_i-1].exchange_1_price}"
+    elsif input == "2"
+      @@selection.exchange_2
+    end
   end
 
 
