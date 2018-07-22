e8e28ae31720bd290b6444b0207518122e07fd2d
Edsel Caprice <caprice@mail.usf.edu>
2018-07-22 00:38:22 +0000

    add gems

--- lib/crypto_cli_app.rb
+++ lib/crypto_cli_app.rb
@@ -1,4 +1,8 @@
 #acts as enironment file
+gem 'pry'
+gem 'nokogiri', '1.6.6.2'
+gem 'open-uri'
+
 require_relative "./crypto_cli_app/version"
 require_relative "./crypto_cli_app/cli"
 require_relative "./crypto_cli_app/crypto"
